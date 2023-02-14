4a8b181a403c2e9f0ddf49d9debc92017ba1d96a
Евгений Галимсаров <address@email.com>
2023-02-14 10:34:48 +0500

    m2l3 инфраструктура по тестам

---
+++ m2l3/build.gradle.kts
@@ -0,0 +1,15 @@
+val kotestVersion: String by project
+
+plugins {
+    kotlin("jvm")
+}
+
+dependencies {
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
