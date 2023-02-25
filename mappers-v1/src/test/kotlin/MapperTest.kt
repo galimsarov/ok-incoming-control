5fcc3280f30260072189e6d57796e002b31db695
evgenii <e.galimsarov@gmail.com>
2023-02-25 12:12:34 +0500

    Реализован маппер toTransport

--- mappers-v1/src/test/kotlin/MapperTest.kt
+++ mappers-v1/src/test/kotlin/MapperTest.kt
@@ -1,5 +1,4 @@
-import models.IctrlVisibility
-import models.IctrlWorkMode
+import models.*
 import org.junit.Test
 import ru.otus.otuskotlin.api.v1.models.*
 import stubs.IctrlStubs
@@ -28,6 +27,44 @@ class MapperTest {
         assertEquals(IctrlStubs.SUCCESS, context.stubCase)
         assertEquals(IctrlWorkMode.STUB, context.workMode)
         assertEquals("name", context.commodityRequest.name)
+        assertEquals("desc", context.commodityRequest.description)
+        assertEquals("12", context.commodityRequest.receiptQuantity)
         assertEquals(IctrlVisibility.VISIBLE_PUBLIC, context.commodityRequest.visibility)
+    }
+
+    @Test
+    fun toTransport() {
+        val context = IctrlContext(
+            requestId = IctrlRequestId("1234"),
+            command = IctrlCommand.CREATE,
+            commodityResponse = IctrlCommodity(
+                name = "name",
+                description = "desc",
+                receiptQuantity = "12",
+                visibility = IctrlVisibility.VISIBLE_PUBLIC,
+            ),
+            errors = mutableListOf(
+                IctrlError(
+                    code = "err",
+                    group = "request",
+                    field = "name",
+                    message = "wrong name",
+                )
+            ),
+            state = IctrlState.RUNNING,
+        )
+
+        val req = context.toTransportCommodity() as CommodityCreateResponse
+
+        assertEquals("1234", req.requestId)
+        assertEquals("name", req.commodity?.name)
+        assertEquals("desc", req.commodity?.description)
+        assertEquals(CommodityVisibility.PUBLIC, req.commodity?.visibility)
+        assertEquals("12", req.commodity?.receiptQuantity)
+        assertEquals(1, req.errors?.size)
+        assertEquals("err", req.errors?.firstOrNull()?.code)
+        assertEquals("request", req.errors?.firstOrNull()?.group)
+        assertEquals("name", req.errors?.firstOrNull()?.field)
+        assertEquals("wrong name", req.errors?.firstOrNull()?.message)
     }
 }
