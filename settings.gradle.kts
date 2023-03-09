d6adc25091ff1815d61f1720e29a172187f5d858
evgenii <e.galimsarov@gmail.com>
2023-03-09 13:16:05 +0500

    Добавлен модуль с spring

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,12 +3,21 @@ rootProject.name = "ok-incoming-control"
 pluginManagement {
     val kotlinVersion: String by settings
     val openapiVersion: String by settings
+    val springframeworkBootVersion: String by settings
+    val springDependencyManagementVersion: String by settings
+    val pluginSpringVersion: String by settings
+    val pluginJpa: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
         kotlin("plugin.serialization") version kotlinVersion apply false
 
         id("org.openapi.generator") version openapiVersion apply false
+
+        id("org.springframework.boot") version springframeworkBootVersion apply false
+        id("io.spring.dependency-management") version springDependencyManagementVersion apply false
+        kotlin("plugin.spring") version pluginSpringVersion apply false
+        kotlin("plugin.jpa") version pluginJpa apply false
     }
 }
 
@@ -24,3 +33,4 @@ include("common")
 include("mappers-v1")
 
 include("stubs")
+include("app-spring")
