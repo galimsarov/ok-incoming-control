c669d9ad5d84621e871bf0c98fbef0f4437fc37f
evgenii <e.galimsarov@gmail.com>
2023-03-13 14:38:17 +0500

    Модуль spring -> модуль ktor

---
+++ app-ktor/build.gradle.kts
@@ -0,0 +1,64 @@
+import org.jetbrains.kotlin.util.suffixIfNot
+
+val ktorVersion: String by project
+val logbackVersion: String by project
+
+fun ktor(module: String, prefix: String = "server-", version: String? = this@Build_gradle.ktorVersion): Any =
+    "io.ktor:ktor-${prefix.suffixIfNot("-")}$module:$version"
+
+plugins {
+    id("application")
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+}
+
+application {
+    mainClass.set("ru.otus.otuskotlin.incomingControl.ApplicationKt")
+}
+
+dependencies {
+    val kotestVersion: String by project
+
+    implementation(ktor("core")) // "io.ktor:ktor-server-core:$ktorVersion"
+    implementation(ktor("netty")) // "io.ktor:ktor-ktor-server-netty:$ktorVersion"
+
+    // jackson
+    implementation(ktor("jackson", "serialization")) // io.ktor:ktor-serialization-jackson
+    implementation(ktor("content-negotiation")) // io.ktor:ktor-server-content-negotiation
+    implementation(ktor("kotlinx-json", "serialization")) // io.ktor:ktor-serialization-kotlinx-json
+
+    implementation(ktor("locations"))
+    implementation(ktor("caching-headers"))
+    implementation(ktor("call-logging"))
+    implementation(ktor("auto-head-response"))
+    implementation(ktor("cors")) // "io.ktor:ktor-cors:$ktorVersion"
+    implementation(ktor("default-headers")) // "io.ktor:ktor-cors:$ktorVersion"
+    implementation(ktor("cors")) // "io.ktor:ktor-cors:$ktorVersion"
+    implementation(ktor("auto-head-response"))
+
+    implementation(ktor("websockets")) // "io.ktor:ktor-websockets:$ktorVersion"
+    implementation(ktor("auth")) // "io.ktor:ktor-auth:$ktorVersion"
+    implementation(ktor("auth-jwt")) // "io.ktor:ktor-auth-jwt:$ktorVersion"
+
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    // transport models
+    implementation(project(":common"))
+
+    // v1 api
+    implementation(project(":api-v1-jackson"))
+    implementation(project(":mappers-v1"))
+
+    // Stubs
+    implementation(project(":stubs"))
+
+    // tests
+    implementation(kotlin("test-junit"))
+    implementation(ktor("test-host")) // "io.ktor:ktor-server-test-host:$ktorVersion"
+    implementation(ktor("content-negotiation", prefix = "client-"))
+    implementation(ktor("websockets", prefix = "client-"))
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,10 +3,7 @@ rootProject.name = "ok-incoming-control"
 pluginManagement {
     val kotlinVersion: String by settings
     val openapiVersion: String by settings
-    val springframeworkBootVersion: String by settings
-    val springDependencyManagementVersion: String by settings
-    val pluginSpringVersion: String by settings
-    val pluginJpa: String by settings
+    val ktorPluginVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
@@ -14,10 +11,7 @@ pluginManagement {
 
         id("org.openapi.generator") version openapiVersion apply false
 
-        id("org.springframework.boot") version springframeworkBootVersion apply false
-        id("io.spring.dependency-management") version springDependencyManagementVersion apply false
-        kotlin("plugin.spring") version pluginSpringVersion apply false
-        kotlin("plugin.jpa") version pluginJpa apply false
+        id("io.ktor.plugin") version ktorPluginVersion apply false
     }
 }
 
@@ -33,4 +27,4 @@ include("common")
 include("mappers-v1")
 
 include("stubs")
-include("app-spring")
+include("app-ktor")
