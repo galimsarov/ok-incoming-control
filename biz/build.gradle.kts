5141d37f0ebd2c3ccfe33cb70b042a2c594e0dd8
Евгений Галимсаров <address@email.com>
2023-05-30 13:07:45 +0500

    CommodityCrudAuthTest добавлен

--- biz/build.gradle.kts
+++ biz/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation(project(":repo-tests"))
     implementation(project(":repo-stubs"))
     implementation(project(":auth"))
+    implementation(project(":repo-in-memory"))
 
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:$coroutinesVersion")
